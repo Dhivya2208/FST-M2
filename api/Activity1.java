6eacbc8e6f142d41409251df300464245a8694de
Dhivya2208 <98084704+Dhivya2208@users.noreply.github.com>
2022-09-05 20:01:20 +0530

    FST M2

---
+++ api/Activity1.java
@@ -0,0 +1,50 @@
+import org.testng.annotations.Test;
+
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import static io.restassured.RestAssured.given;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+
+public class Activity1 {
+	
+	final static String ROOT_URI = "https://petstore.swagger.io/v2/pet";
+	
+  @Test(priority = 1)
+  public void postRequest() {
+	  String reqBody = "{"
+	            + "\"id\": 77232,"
+	            + "\"name\": \"Riley\","
+	            + " \"status\": \"alive\""
+	        + "}";
+	  
+	  Response response = given().contentType(ContentType.JSON)
+			  .body(reqBody)
+			  .when().post(ROOT_URI);
+	  
+	  response.then().body("id", equalTo(77232));
+      response.then().body("name", equalTo("Riley"));
+      response.then().body("status", equalTo("alive"));
+  }
+  
+  @Test(priority = 2)
+  public void getRequest() {
+	  Response response = given().contentType(ContentType.JSON)
+			  .when().pathParam("petId", "77232")
+			  .get(ROOT_URI + "/{petId}");
+	  
+	  response.then().body("id", equalTo(77232));
+      response.then().body("name", equalTo("Riley"));
+      response.then().body("status", equalTo("alive"));
+  }
+  
+  @Test(priority = 3)
+  public void deleteRequest() {
+	  Response response = given().contentType(ContentType.JSON)
+			  .when().pathParam("petId", "77232")
+			  .delete(ROOT_URI + "/{petId}");
+	  
+	  response.then().body("code", equalTo(200));
+      response.then().body("message", equalTo("77232"));
+  }
+}
